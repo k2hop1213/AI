2e94386fe8d0e7f95566e1679229d9814a53ba29
KyuHyun Kim <140415177+k2hop1213@users.noreply.github.com>
2026-08-05 21:16:35 +0900

    Update MLP.c

--- MLP.c
+++ MLP.c
@@ -1,7 +1,10 @@
 /*
 저번주차에 손계산한 결과와 동일한지 확인하기 위해
 계산 편의상 사용했던 반올림 과정을 넣었습니다
-반올림한 부분을 주석처리하고 반복횟수를 늘리면 가중치가 더욱 정교하게 업데이트 되고 오차가 줄어드는것을 확인할 수 있었습니다.
+반올림 여부는 아래 ROUND_FOR_COMPARISON을 0으로 바꿔서 끌 수 있습니다.
+반올림을 끄고 반복횟수를 늘리면 가중치가 더욱 정교하게 업데이트 되고 오차가 줄어드는것을 확인할 수 있었습니다.
+(반올림을 켠 상태에서는 작은 델타 값이 0으로 잘려서, 일부 가중치의 기울기가 소실되어
+ 업데이트가 멈추는 현상이 나타남 - 결과 해석 시 참고)
 
 신경망의 구성을 자유롭게 하기 위해 동적할당을 적극 활용하여 코드를 짰습니다.
 */
@@ -22,7 +25,7 @@
 
 0.5 0.3 0.8
 
-0.1 0.2 0.3
+0.1 0.2 0.37
 0.4 0.5 0.6
 0.7 0.8 0.9
 
@@ -45,6 +48,14 @@
 #include <stdlib.h>
 #include <math.h>
 
+// 1: 지난 주차 손계산과 비교하기 위해 소수점 둘째 자리로 반올림
+// 0: 반올림 없이 정밀 계산 (가중치가 더 세밀하게 업데이트되고 오차가 더 잘 줄어드는지 비교용)
+#define ROUND_FOR_COMPARISON 1
+
+double r2(double x) { // 반올림 토글 적용
+    return ROUND_FOR_COMPARISON ? round(x * 100.0) / 100.0 : x;
+}
+
 double sigmoid(double x) { //활성화 함수
     return 1.0 / (1.0 + exp(-x));
 
@@ -57,7 +68,7 @@ double sigmoid_deriv(double x) {
 void print_matrix(double** mat, int row, int col) {
     for (int i = 0; i < row; i++) {
         for (int j = 0; j < col; j++) {
-            printf("%lf ", mat[i][j]);
+            printf("%.4lf ", mat[i][j]);
         }
         printf("\n");
     }
@@ -83,7 +94,7 @@ void multiply_matrix(double** a, double** b, double** c, int row1, int col1, int
     }
 }
 
-double** transpose(double** mat, int row,int col) { // 전치행렬을 반환하는 함수
+double** transpose(double** mat, int row, int col) { // 전치행렬을 반환하는 함수
     double** n = malloc(sizeof(double*) * col);
     for (int i = 0; i < col; i++) {
         n[i] = malloc(sizeof(double) * row);
@@ -98,14 +109,14 @@ double** transpose(double** mat, int row,int col) { // 전치행렬을 반환하
 
 
 
-void forward(double*** MLP, double*** weight, int sizes[], int layer,double b) { // 순전파
+void forward(double*** MLP, double*** weight, int sizes[], int layer, double b) { // 순전파
 
     for (int i = 0; i < layer - 1; i++) {
         multiply_matrix(weight[i], MLP[i], MLP[i + 1], sizes[i + 1], sizes[i], 1);
         if (i != layer - 2) {
             for (int j = 0; j < sizes[i + 1]; j++) {
                 MLP[i + 1][j][0] = sigmoid(MLP[i + 1][j][0] + b);
-                MLP[i + 1][j][0] = round(MLP[i + 1][j][0] * 100.0) / 100.0;
+                MLP[i + 1][j][0] = r2(MLP[i + 1][j][0]);
             }
         }
         else {
@@ -117,11 +128,11 @@ void forward(double*** MLP, double*** weight, int sizes[], int layer,double b) {
 
 }
 
-void backward(double*** MLP, double*** weight,double*** trans_weight,double*** dw, double*** delta, double* b, double* target,int sizes[], int layer,double lr) { // 역전파
+void backward(double*** MLP, double*** weight, double*** trans_weight, double*** dw, double*** delta, double* b, double* target, int sizes[], int layer, double lr) { // 역전파
     double db = 0.0; // 편향의 기울기
     for (int i = 0; i < sizes[layer - 1]; i++) {
         delta[layer - 2][i][0] = -2.0 * (target[i] - MLP[layer - 1][i][0]); // 출력층에선 항등함수를 사용했기 때문
-        delta[layer - 2][i][0] = round(delta[layer - 2][i][0] * 100.0) / 100.0;
+        delta[layer - 2][i][0] = r2(delta[layer - 2][i][0]);
         db += delta[layer - 2][i][0];
     }
 
@@ -131,23 +142,22 @@ void backward(double*** MLP, double*** weight,double*** trans_weight,double*** d
         multiply_matrix(trans_weight[i], delta[i], delta[i - 1], sizes[i], sizes[i + 1], 1);
         for (int j = 0; j < sizes[i]; j++) {
             delta[i - 1][j][0] *= sigmoid_deriv(MLP[i][j][0]);
-            delta[i - 1][j][0] = round(delta[i - 1][j][0] * 100.0) / 100.0;
+            delta[i - 1][j][0] = r2(delta[i - 1][j][0]);
             db += delta[i - 1][j][0];
         }
     }
     for (int i = 0; i < layer - 1; i++) {
         double** temp = transpose(MLP[i], sizes[i], 1);
-        multiply_matrix(delta[i], temp,  dw[i], sizes[i + 1], 1, sizes[i]);
-        free_matrix(temp,1);
+        multiply_matrix(delta[i], temp, dw[i], sizes[i + 1], 1, sizes[i]);
+        free_matrix(temp, 1);
     }
     *b -= (lr * db);
-    *b = round(*b * 100.0) / 100.0;
+    *b = r2(*b);
     for (int i = 0; i < layer - 1; i++) {
         for (int j = 0; j < sizes[i + 1]; j++) {
             for (int k = 0; k < sizes[i]; k++) {
-                dw[i][j][k] = round(dw[i][j][k] * 100.0) / 100.0;
-                weight[i][j][k] -= lr * dw[i][j][k];
-                weight[i][j][k] = round(weight[i][j][k] * 100.0) / 100.0;
+                weight[i][j][k] -= lr * r2(dw[i][j][k]);
+                weight[i][j][k] = r2(weight[i][j][k]);
             }
         }
     }
@@ -162,7 +172,7 @@ int main() {
     double*** trans_weight = NULL; // 역전파에서 사용할 전치행렬
     double*** delta = NULL;
     double*** dw = NULL; // 기울기
-    int layer, *sizes;//레이어 개수와 각 레이어의 노드 개수
+    int layer, * sizes;//레이어 개수와 각 레이어의 노드 개수
     // layer - 1 : 가중치 행렬의 개수
     double b;// 편향
     double* target;// 목표값
@@ -209,7 +219,7 @@ int main() {
         }
     }
     scanf("%lf", &b); // 편향
-    
+
     target = malloc(sizeof(double) * (sizes[layer - 1]));
     for (int i = 0; i < sizes[layer - 1]; i++) {
         scanf("%lf", &target[i]);
@@ -217,21 +227,22 @@ int main() {
     double lr = 0.0;
     scanf("%lf", &lr);// 학습률
     double Error = 0.0;
-    
+
     int rep = 0;
-    // 반복횟수 입력받아서 원하는 횟수 만큼 순전파 역전파 반복하기
-    
+    //ver.1 반복횟수 입력받아서 원하는 횟수 만큼 순전파 역전파 반복하기
+
 
     scanf("%d", &rep);// 반복횟수
-   
+
+    double* error_history = malloc(sizeof(double) * (rep + 1)); // 회차별 오차 기록 (마지막 요약용)
 
 
     for (int a = 1; a <= rep; a++) {
         printf("----------------------\n");
 
-        printf("순전파 %d회차\n",a);
+        printf("순전파 %d회차\n", a);
         forward(MLP, weight, sizes, layer, b);
-        printf("입력레이어부터 출력레이어까지 차례대로 출력\n");
+        printf("첫 번째 히든레이어부터 출력레이어까지 차례대로 출력\n");
 
         if (a > 1) { // 전 회차에 할당한 공간이 있으므로 먼저 해제 시키기
             for (int i = 0; i < layer - 1; i++) {
@@ -251,16 +262,17 @@ int main() {
             printf("\n");
 
         }
-      
+
 
         Error = 0.0;
         for (int i = 0; i < sizes[layer - 1]; i++) {
-            Error += pow(target[i] - MLP[layer - 1][i][0],2);
+            Error += pow(target[i] - MLP[layer - 1][i][0], 2);
         }
+        error_history[a - 1] = Error;
         printf("오차 : %lf\n", Error);
         printf("----------------------\n");
 
-        printf("역전파 %d회차\n",a);
+        printf("역전파 %d회차\n", a);
 
         backward(MLP, weight, trans_weight, dw, delta, &b, target, sizes, layer, lr);
         printf("델타 값\n");
@@ -287,7 +299,7 @@ int main() {
 
     printf("순전파 %d회차\n", rep + 1);
     forward(MLP, weight, sizes, layer, b);
-    printf("입력레이어부터 출력레이어까지 차례대로 출력\n");
+    printf("첫 번째 히든레이어부터 출력레이어까지 차례대로 출력\n");
 
     for (int i = 0; i < layer; i++) {
 
@@ -299,9 +311,18 @@ int main() {
     for (int i = 0; i < sizes[layer - 1]; i++) {
         Error += pow(target[i] - MLP[layer - 1][i][0], 2);
     }
+    error_history[rep] = Error;
     printf("오차 : %lf\n", Error);
     printf("----------------------\n");
-    
+
+    printf("\n[오차 변화 요약]\n");
+    for (int a = 0; a <= rep; a++) {
+        printf("%d회차 순전파 오차 : %.6lf\n", a + 1, error_history[a]);
+    }
+    printf("----------------------\n");
+
+    free(error_history);
+
     // 동적 할당 해제
 
     for (int i = 0; i < layer; i++) {
@@ -317,7 +338,7 @@ int main() {
         for (int j = 0; j < sizes[i + 1]; j++) {
             free(weight[i][j]);
             free(dw[i][j]);
-            free(delta[i][j]); 
+            free(delta[i][j]);
         }
         free(weight[i]);
         free(dw[i]);
